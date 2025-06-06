5283909cdc7d929cb05bb4c4ae91c7fbfed0ccce
David Tomas <105463065+DadiGang-1@users.noreply.github.com>
2025-06-06 13:57:23 +0000

    3t add board, square, useState

--- 3t/src/App.js
+++ 3t/src/App.js
@@ -1,23 +1,56 @@
+import { useState } from 'react';
+
 function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+  
+  return (
+    <button 
+      className='square'
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+
+  );
+}
+
+function Board() {
   return (
     <>
       <div className="board-row">
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className='square'>X</button>
-        <button className='square'>X</button>
-        <button className='square'>X</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
 
-export default Square;
+export default Board;
+
+
+function Row() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
